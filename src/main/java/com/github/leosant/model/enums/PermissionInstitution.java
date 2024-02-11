ad271ea06da6a97b7f4bc357e71ff94bdc76c763
leosant <https://github.com/leosant>
2024-02-11 12:49:04 -0300

    Renamed of class PermissionInstitution

--- src/main/java/com/github/leosant/model/enums/PermissionInstitution.java
+++ src/main/java/com/github/leosant/model/enums/PermissionInstitution.java
@@ -2,19 +2,19 @@ package com.github.leosant.model.enums;
 
 import org.apache.commons.lang3.StringUtils;
 @SuppressWarnings(value = "unused")
-public enum PermissionInstituction {
+public enum PermissionInstitution {
 
     SANTANDER("santander");
 
     private final String name;
 
-    PermissionInstituction(String name) {
+    PermissionInstitution(String name) {
         this.name = name;
     }
 
     public static boolean isValid(String name) {
-        for (PermissionInstituction permissionInstituction : PermissionInstituction.values()) {
-            if (StringUtils.containsIgnoreCase(name, permissionInstituction.name)) {
+        for (PermissionInstitution permissionInstitution : PermissionInstitution.values()) {
+            if (StringUtils.containsIgnoreCase(name, permissionInstitution.name)) {
                 return true;
             }
         }
